5ffe6b332301c8404f2b2d666a3c31864089d48e
elternative <jinghu_tsinghua@foxmail.com>
2016-07-31 22:59:19 +0800

    ReadIn function building

--- dat.c
+++ dat.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+int Readin(){
+  FILE *ptr_file=NULL;
+  long SZFile;
+  int SiZfile;
+  ptr_file=fopen("dat.bin","rb");
+  if (NULL==ptr_file) {
+    return -1;
+  }
+  fseek(ptr_file,0L,SEEK_END);
+  SZFile=ftell(ptr_file);
+  SiZfile=int(SZFile);
+  int NumArr=(SiZfile-4)/2;
+  rewind(ptr_file);
+  int *buff=(int *)malloc(SiZfile*sizeof(int));
 
+  void * _buffB;
+}
 int main(){
   printf("hello world");
   return 0;
